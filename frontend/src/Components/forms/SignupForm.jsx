c7eb04d7eca3e4d828983192c007240ae4699cce
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-03-09 00:35:18 -0500

    Form signup working

--- frontend/src/Components/forms/SignupForm.jsx
+++ frontend/src/Components/forms/SignupForm.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
+import { useStateContext } from "../../contexts/ContextProvider";
+import axiosClient from "../../helpers/AxiosClient";
 import { useForm } from "../../hooks/useForm";
 
 export const SignupForm = () => {
+
+  const {setUser, setToken} = useStateContext()
 
   const {name, email, password, onInputChange, onResetForm} = useForm({
     name: "",
@@ -9,25 +14,51 @@ export const SignupForm = () => {
     password: "",
   })
 
-  const onSubmit = (e) => {
+  const [errors, setErrors] = useState()
+
+  const onSubmit = async (e) => {
+    setErrors()
     e.preventDefault();
-    console.log({name, email, password})
+    const payload = {
+      name,
+      email,
+      password
+    };
+    try {
+      const {status, data } = await axiosClient.post("/signup", payload);
+      if(status === 200){
+        setUser(data.user);
+        setToken(data.token);
+      }
+    } catch (error) {
+      const {errors} = error.response.data
+      setErrors(errors)
+    }
   }
 
   return (
     <form className="border border-dark rounded col-6 bg-white p-4 row justify-content-center" onSubmit={onSubmit}>
       <h1 className="text-center">Login</h1>
+      {errors &&
+        <div className="alert alert-danger">
+          <ul>
+            {Object.keys(errors).map(key => (
+              <li key={key}>{errors[key][0]}</li>
+            ))}
+          </ul>
+        </div>
+      }
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input type="text" className="form-control" id="name" name="name" onChange={onInputChange} value={name} required/>
+        <input type="text" className="form-control" id="name" name="name" onChange={onInputChange} value={name} />
       </div>
       <div className="mb-3">
         <label htmlFor="email" className="form-label">Email</label>
-        <input type="email" className="form-control" id="email" name="email" onChange={onInputChange} value={email} required/>
+        <input type="email" className="form-control" id="email" name="email" onChange={onInputChange} value={email} />
       </div>
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password</label>
-        <input type="password" className="form-control" id="password" name="password" onChange={onInputChange} value={password} required/>
+        <input type="password" className="form-control" id="password" name="password" onChange={onInputChange} value={password} />
       </div>
       <div className="mb-3 text-center">
         <small>Do you have an account? <Link to={"/login"}>Login here</Link></small>
