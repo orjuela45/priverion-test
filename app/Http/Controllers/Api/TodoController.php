27545f0c279035a51afbd45799dec94d584ae71c
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-03-09 09:27:20 -0500

    Create controller, requests and resource for TODO. Also create endpoint to get users todo

---
+++ app/Http/Controllers/Api/TodoController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Todo;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTodoRequest;
+use App\Http\Requests\UpdateTodoRequest;
+use App\Http\Resources\TodoResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user  = Auth::user();
+        $todos = User::find($user->id)->todos;
+        return TodoResource::collection($todos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTodoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTodoRequest $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
