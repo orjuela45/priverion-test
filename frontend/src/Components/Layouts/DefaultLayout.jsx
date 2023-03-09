69e5eee3e4571e193d4516475e747100b7050a89
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-03-08 23:42:29 -0500

    redirects to layout according to token

--- frontend/src/Components/Layouts/DefaultLayout.jsx
+++ frontend/src/Components/Layouts/DefaultLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../../contexts/ContextProvider"
 
 export const DefaultLayout = () => {
+
+  const {token} = useStateContext();
+
+  if(!token){
+    return <Navigate to={"/login"} />
+  }
+
   return (
     <div>
       DefaultLayout
--- frontend/src/Components/Layouts/GuestLayout.jsx
+++ frontend/src/Components/Layouts/GuestLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../../contexts/ContextProvider";
 
 export const GuestLayout = () => {
+
+  const {token} = useStateContext();
+
+  if(token){
+    return <Navigate to={"/"} />
+  }
+  
   return (
     <div>
       GuestLayout
