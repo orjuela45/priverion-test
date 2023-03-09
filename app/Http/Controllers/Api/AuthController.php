5ec2b02ddea50964b84f3db2d04a26de7ea9533b
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-03-08 22:56:38 -0500

    Create login backend

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -16,7 +18,24 @@ class AuthController extends Controller
             "email" => $data["email"],
             "password" => bcrypt($data["password"]),
         ]);
+        /** @var User $user */
         $token = $user->createToken('main')->plainTextToken;
+        return response([
+            "user"  => $user,
+            "token" => $token
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)){
+            return response([
+                "message" => "Credentials invalid"
+            ], 401);
+        }
+        /** @var User $user */
+        $user  = Auth::user();
+        $token = $user->createToken("main")->plainTextToken;
         return response([
             "user"  => $user,
             "token" => $token
