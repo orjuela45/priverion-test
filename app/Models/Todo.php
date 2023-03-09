5c81be09d7f4f7f9fad566b006b72ec4ae19dabf
Miguel Orjuela <39718400+orjuela45@users.noreply.github.com>
2023-03-09 08:52:55 -0500

    create model and migration of TODOS and add relation whit user

---
+++ app/Models/Todo.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'todo_id';
+
+    protected $fillable = [
+        "title",
+        "description",
+        "status",
+        "public",
+        "deleted"
+    ];
+
+    public function closeTask()
+    {
+        $this->update([
+            $this->status = 0 
+        ]);
+    }
+    
+    public function changePublicTask()
+    {
+        $this->update([
+            $this->public = !$this->public
+        ]);
+    }
+
+    public function deleteTask()
+    {
+        $this->update([
+            $this->deleted = 0 
+        ]);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
